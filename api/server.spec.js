e60101273ba6e10266ab3fa390c4efc5336e1f1a
Zolton <gmzesq@gmail.com>
2019-09-16 18:45:19 -0400

    Supertests pass.  But having problems with deleting the test data from the server

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,7 +1,6 @@
 const server = require("./server");
 const request = require("supertest");
 
-
 // Request to root "/" - response on site should be "Welcome to Our Server (Geograpics)"
 describe("GET root", () => {
   it("GET server.js", () => {
@@ -17,19 +16,59 @@ describe("User CRUD tests", () => {
   it("GET list of users", async () => {
     return await request(server)
       .get("/users")
-      .expect(200)
-  })
+      .expect(200);
+  });
 
-  // it("POST new User", async () => {
-  //   return await request(server)
-  //     .post("/users")
-  //     .send({
-  //       insta_id: 123456,
-  //       access_token: "TestUserToken",
-  //       private: true,
-  //       username: "TestUsername",
-  //       full_name: "Test Fullname"
-  //     })
-  //     .expect(200)
-  // })
+  it("POST new User + delete User", async () => {
+    return await request(server)
+      .post("/users")
+      .send({
+        insta_id: 1,
+        access_token: "TestUserToken",
+        private: true,
+        username: "TestUsername",
+        full_name: "Test Fullname"
+      })
+      .expect(200)
+      .then(res => {
+        let userID = res;
+        request(server)
+        .delete("/users")
+        .query({ id: userID })
+        .expect(200)
+          })
+      })
+
+
+  it("POST new User + PUT + Delete User", async () => {
+    return await request(server)
+      .post("/users")
+      .send({
+        insta_id: 2,
+        access_token: "User Access Token",
+        private: true,
+        username: "Username",
+        full_name: "Fullname"
+      })
+      .expect(200)
+      .then(res => {
+        let userID = res;
+        request(server)
+          .put("/users")
+          .send({
+            insta_id: 3,
+            access_token: "TestUserTokenEdited",
+            private: true,
+            username: "TestUsernameEdited",
+            full_name: "Test FullnameEdited"
+          })
+          .expect(200)
+          .then(res2 => {
+            request(server)
+              .delete("/users")
+              .query({ id: userID })
+              .expect(200)
+          })
+      })
+  })
 })
--- userRouter/userRouter.js
+++ userRouter/userRouter.js
@@ -1,49 +1,49 @@
 const express = require("express");
 const router = express.Router();
-const helper = require("./userHelper")
+const helper = require("./userHelper");
 
 // Retrieve list of all users
 // Comment out before production
-router.get("/", (req, res)=>{
-    helper.findAllUsers()
-    .then(users=>{
-        res.status(200).json(users)
+router.get("/", (req, res) => {
+  helper
+    .findAllUsers()
+    .then(users => {
+      res.status(200).json(users);
     })
-    .catch(error=>{
-        res.status(500).json({Error: "Error"})
-    })
-})
+    .catch(error => {
+      res.status(500).json({ Error: "Error" });
+    });
+});
 
 // Insert new user into DB
-router.post("/", (req, res)=>{
-    let newUserInfo = req.body
-    helper.postNewUser(newUserInfo)
-    .then(newUser=>{
-        res.status(200).json(newUser)
+router.post("/", (req, res) => {
+  let newUserInfo = req.body;
+  helper
+    .postNewUser(newUserInfo)
+    .then(newUser => {
+      res.status(200).json(newUser);
     })
-    .catch(error=>{
-        res.status(500).json({Error: "Error"})
-    })
-})
+    .catch(error => {
+      res.status(500).json({ Error: "Error" });
+    });
+});
 
-router.put("/:id", (req, res)=>{
-    let id = req.params.id
-    let editInfo = req.body
-    helper.editUser(id, editInfo)
-    .then(edited=>{
-        res.status(200).json(edited)
-    })
-})
+// Edit user info - ALL user info, must fill in all fields
+// regardless if they change or not
+router.put("/:id", (req, res) => {
+  let id = req.params.id;
+  let editInfo = req.body;
+  helper.editUser(id, editInfo).then(edited => {
+    res.status(200).json(edited);
+  });
+});
 
+// Delete user
+router.delete("/:id", (req, res) => {
+  let id = req.params.id;
+  helper.deleteUser(id).then(deleted => {
+    res.status(200).json(deleted);
+  });
+});
 
-router.delete("/:id", (req, res)=>{
-    let id = req.params.id
-    helper.deleteUser(id)
-    .then(deleted=>{
-        res.status(200).json(deleted)
-    })
-})
-
-
-
-module.exports = router
+module.exports = router;
