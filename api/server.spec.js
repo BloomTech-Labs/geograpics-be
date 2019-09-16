672f4c6dc2a0ad17e24dc796f23bd492a2eeac1e
Zolton <gmzesq@gmail.com>
2019-09-16 18:52:18 -0400

    WIP - Supertest POST and PUT work.  Delete still problematic

--- api/server.spec.js
+++ api/server.spec.js
@@ -17,58 +17,71 @@ describe("User CRUD tests", () => {
     return await request(server)
       .get("/users")
       .expect(200);
-  });
+  })
 
-  it("POST new User + delete User", async () => {
+  it("POST new User + PUT User", async () => {
     return await request(server)
       .post("/users")
       .send({
         insta_id: 1,
-        access_token: "TestUserToken",
+        access_token: "TestUserToken2",
         private: true,
-        username: "TestUsername",
-        full_name: "Test Fullname"
+        username: "TestUsername2",
+        full_name: "Test Fullname2"
       })
       .expect(200)
       .then(res => {
-        let userID = res;
-        request(server)
-        .delete("/users")
-        .query({ id: userID })
-        .expect(200)
-          })
+              let userID = res;
+              request(server)
+                .put("/users")
+                .send({
+                  insta_id: 3,
+                  access_token: "TestUserTokenEdited",
+                  private: true,
+                  username: "TestUsernameEdited",
+                  full_name: "Test FullnameEdited"
+                })
+                .expect(200)
+      // .then(res => {
+      //   let userID = res;
+      //   request(server)
+      //   .delete("/users")
+      //   .query({ id: userID })
+      //   .del()
+      //   .expect(200)
+      //     })
       })
 
 
-  it("POST new User + PUT + Delete User", async () => {
-    return await request(server)
-      .post("/users")
-      .send({
-        insta_id: 2,
-        access_token: "User Access Token",
-        private: true,
-        username: "Username",
-        full_name: "Fullname"
-      })
-      .expect(200)
-      .then(res => {
-        let userID = res;
-        request(server)
-          .put("/users")
-          .send({
-            insta_id: 3,
-            access_token: "TestUserTokenEdited",
-            private: true,
-            username: "TestUsernameEdited",
-            full_name: "Test FullnameEdited"
-          })
-          .expect(200)
-          .then(res2 => {
-            request(server)
-              .delete("/users")
-              .query({ id: userID })
-              .expect(200)
-          })
-      })
-  })
+  // it("POST new User + PUT + Delete User", async () => {
+  //   return await request(server)
+  //     .post("/users")
+  //     .send({
+  //       insta_id: 2,
+  //       access_token: "User Access Token",
+  //       private: true,
+  //       username: "Username",
+  //       full_name: "Fullname"
+  //     })
+  //     .expect(200)
+  //     .then(res => {
+  //       let userID = res;
+  //       request(server)
+  //         .put("/users")
+  //         .send({
+  //           insta_id: 3,
+  //           access_token: "TestUserTokenEdited",
+  //           private: true,
+  //           username: "TestUsernameEdited",
+  //           full_name: "Test FullnameEdited"
+  //         })
+  //         .expect(200)
+  //         .then(res2 => {
+  //           request(server)
+  //             .delete("/users")
+  //             .query({ id: userID })
+  //             .expect(200)
+  //         })
+  //     })
+   })
 })
