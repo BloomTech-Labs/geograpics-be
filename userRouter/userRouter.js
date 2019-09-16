800c72b1d821e900d6080c4cbc21b524523aed3e
Zolton <gmzesq@gmail.com>
2019-09-16 19:06:47 -0400

    Added in missing .catch error messages

--- userRouter/userRouter.js
+++ userRouter/userRouter.js
@@ -11,9 +11,9 @@ router.get("/", (req, res) => {
       res.status(200).json(users);
     })
     .catch(error => {
-      res.status(500).json({ Error: "Error" });
-    });
-});
+      res.status(500).json({ Error: "Failed to retrieve users" });
+    })
+})
 
 // Insert new user into DB
 // sends new user ID back
@@ -25,9 +25,9 @@ router.post("/", (req, res) => {
       res.status(200).json(newUser);
     })
     .catch(error => {
-      res.status(500).json({ Error: "Error" });
-    });
-});
+      res.status(500).json({ Error: "Failed to add new user" });
+    })
+})
 
 // Edit user info - ALL user info, must fill in all fields
 // regardless if they change or not
@@ -38,15 +38,22 @@ router.put("/:id", (req, res) => {
   let editInfo = req.body;
   helper.editUser(id, editInfo).then(edited => {
     res.status(200).json(edited);
-  });
-});
+  })
+  .catch(error => {
+    res.status(500).json({ Error: "Failed to edit user" });
+  })
+})
 
 // Delete user - id must be in URL parameter string
 router.delete("/:id", (req, res) => {
   let id = req.params.id;
-  helper.deleteUser(id).then(deleted => {
+  helper.deleteUser(id)
+  .then(deleted => {
     res.status(200).json(deleted);
-  });
-});
+  })
+  .catch(error => {
+    res.status(500).json({ Error: "Failed to delete user" });
+  })
+})
 
 module.exports = router;
