c7cb210c0423b6e1ec65b348ccb5e8e5b90341cf
bwpeck <benjaminwade483@yahoo.com>
2019-09-17 11:39:32 -0400

    cypress added

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -11,7 +11,7 @@
 //
 // You can read more here:
 // https://on.cypress.io/configuration
-// ***********************************************************
+// *************************************************************
 
 // Import commands.js using ES2015 syntax:
 import './commands'
