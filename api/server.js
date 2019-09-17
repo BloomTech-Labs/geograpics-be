63d79bfb6cd9b4b07e291fde24bce60fbfdd431e
Zolton <gmzesq@gmail.com>
2019-09-17 16:33:19 -0400

    Added passport setup code from source article in comments.  Added supertest code from Dustin Hamano that should allow supertest to delete its db calls, still need to implement though

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const session = require("express-session");
 const passport = require("passport");
 const instagram = require("passport-instagram");
+const instagramStrategy = instagram.Strategy;
 const axios = require("axios");
 
 const cors = require("cors");
@@ -16,8 +17,48 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 server.use(cors());
+
+// From here to end, all boilerplate code to get passport to work
+// Source: https://dev.to/aurelkurtula/working-with-instagram-api-and-passportjs-in-a-node-application--5068
 server.use(express.static(__dirname + "/public"));
+
 server.set("view engine", "pug");
+
+server.use(
+  session({
+    // Tribute to creator of Lupin the Third
+    name: "Monkey Punch",
+    secret:
+      process.env.PASSPORT_SECRET || "Be more like Goemon, and just shut up",
+    resave: false,
+    saveUninitialized: true
+  })
+);
+
+server.use(passport.initialize());
+server.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.use(
+  new instagramStrategy(
+    {
+      clientID: "...",
+      clientSecret: "...",
+      callbackURL: "http://localhost:3000/auth/instagram/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      done(null, profile);
+    }
+  )
+);
+
+// End passport setup
 
 server.use("/users", userRouter);
 server.use("/map", pictureRouter);
--- api/server.spec.js
+++ api/server.spec.js
@@ -17,31 +17,32 @@ describe("User CRUD tests", () => {
     return await request(server)
       .get("/users")
       .expect(200);
-  })
+  });
 
-  it("POST new User + PUT User", async () => {
+  it("POST new User + PUT User + DELETE User", async () => {
     return await request(server)
       .post("/users")
       .send({
         insta_id: 1,
-        access_token: "TestUserToken2",
+        access_token: "TestUserToken",
         private: true,
-        username: "TestUsername2",
-        full_name: "Test Fullname2"
+        username: "TestUsername",
+        full_name: "Test Fullname"
       })
       .expect(200)
       .then(res => {
-              let userID = res;
-              request(server)
-                .put("/users")
-                .send({
-                  insta_id: 3,
-                  access_token: "TestUserTokenEdited",
-                  private: true,
-                  username: "TestUsernameEdited",
-                  full_name: "Test FullnameEdited"
-                })
-                .expect(200)
+        let userID = res;
+        request(server)
+          .put("/users")
+          .send({
+            insta_id: 2,
+            access_token: "TestUserTokenEdited",
+            private: true,
+            username: "TestUsernameEdited",
+            full_name: "Test FullnameEdited"
+          })
+          .expect(200);
+      })
       // .then(res => {
       //   let userID = res;
       //   request(server)
@@ -50,38 +51,61 @@ describe("User CRUD tests", () => {
       //   .del()
       //   .expect(200)
       //     })
-      })
+
+      //         .then(res2 => {
+      //           request(server)
+      //             .delete("/users")
+      //             .query({ id: userID })
+      //             .expect(200)
+      //         })
+      //     })
+
+      .then(res2 => {
+        request(server)
+          .delete(`/users/${userID}`)
+          .expect(200);
+      });
+
+  });
+});
+
+// From Dustin Hamano: 
 
 
-  // it("POST new User + PUT + Delete User", async () => {
-  //   return await request(server)
-  //     .post("/users")
-  //     .send({
-  //       insta_id: 2,
-  //       access_token: "User Access Token",
-  //       private: true,
-  //       username: "Username",
-  //       full_name: "Fullname"
-  //     })
-  //     .expect(200)
-  //     .then(res => {
-  //       let userID = res;
-  //       request(server)
-  //         .put("/users")
-  //         .send({
-  //           insta_id: 3,
-  //           access_token: "TestUserTokenEdited",
-  //           private: true,
-  //           username: "TestUsernameEdited",
-  //           full_name: "Test FullnameEdited"
-  //         })
-  //         .expect(200)
-  //         .then(res2 => {
-  //           request(server)
-  //             .delete("/users")
-  //             .query({ id: userID })
-  //             .expect(200)
-  //         })
-  //     })
-   })
-})
+// describe("DELETE /auth/journal/:id", () => {
+//   it("returns 401 Unauthorized", async () => {
+//     let arr = await db("journal");
+//     let id = arr[arr.length - 1].id;
+
+//     return request(server)
+//       .delete(`/auth/journal/${id}`)
+//       .then(res => {
+//         expect(res.status).toBe(401);
+//       });
+//   });
+
+//   //  Code to DELETE from Dustin Hamano
+//   it("returns 204 No Content Success", async () => {
+//     let arr = await db("journal");
+//     let id = arr[arr.length - 1].id;
+
+//     return request(server)
+//       .delete(`/auth/journal/${id}`)
+//       .set("Authorization", token)
+//       .then(res => {
+//         expect(res.status).toBe(204);
+//       });
+//   });
+
+//   it("removes from the database", async () => {
+//     let arr = await db("journal");
+//     let id = arr[arr.length - 1].id;
+
+//     let before = await db("journal");
+//     await request(server)
+//       .delete(`/auth/journal/${id}`)
+//       .set("Authorization", token);
+//     let after = await db("journal");
+//     expect(before.length - after.length).toBe(1);
+//   });
+// })
