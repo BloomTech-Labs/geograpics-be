744d3763e4b7da294f5dfb386ab53bdb66cb706f
Zolton <gmzesq@gmail.com>
2019-10-01 16:49:01 -0400

    test

--- index.js
+++
@@ -1,8 +0,0 @@
-const server = require("./api/server");
-require("dotenv").config();
-
-const PORT = process.env.PORT || 5500;
-
-server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
-});
